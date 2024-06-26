5b5ebcafe580566a4e42f4d045feb2bf048ffad3
Nivedita <nivedita.ganesh.22@outlook.com>
2024-06-26 19:38:44 +0530

    Initial commit

---
+++ 03.variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+  type        = string
+  description = "AMI of instance"
+  default     = "ami-0705384c0b33c194c"
+}
+variable "instance_type" {
+  description = "type of instance used"
+  type        = string
+  default     = "t3.micro"
+}
